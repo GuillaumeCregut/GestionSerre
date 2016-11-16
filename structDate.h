b5eb2fd397cb9a5698faf661efe458df0cd7e8bb
guillaumeC37 <warthog@free.fr>
2016-11-16 22:43:56 +0100

    Ajout commentaires

--- structDate.h
+++ structDate.h
@@ -1,3 +1,13 @@
+/* La strutucture suivante "DateRTC" permet de manipuler facilement les données de date / heure
+ *  Date date;
+ *   date.mois = 1;
+ *
+ * Attention ! Lorsque la structure est passé en argument via un pointeur, ou qu'on accède à celle ci via un pointeur il faut utiliser ->
+ * Exemple : Date *date, date2;
+ *           date = &date2; // le pointeur date (*date) pointe désormais sur l'adresse de date2 (&date2)
+ *           date->mois = 1; // correspond à date2.mois = 1;
+ * /!\ Quand une strucuture ou une variable est passé en référence via un pointeur à une fonction c'est la variable pointé qui est modifié ET NON UNE COPIE de la variable comme c'est le cas dans un passage d'arguments normal.  
+ */
 typedef struct {
   uint8_t secondes;
   uint8_t minutes;
