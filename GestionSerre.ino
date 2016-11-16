f8b5e7810e50d1f8bd71fb052404e1fdfc35739a
guillaumeC37 <warthog@free.fr>
2016-11-16 21:03:43 +0100

    Ajout fonctions horaires

--- GestionSerre.ino
+++ GestionSerre.ino
@@ -2,16 +2,29 @@
 /*            Gestion Serre intelligente             */
 /*   G. Cregut                                       */
 /*   DATE Création : 14/11/2016                      */
-/*   Date Modification : 15/11/2016                  */
+/*   Date Modification : 16/11/2016                  */
 /* (c)2016 Editiel98                                 */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * */
-//déclarations includes
 
+//déclarations includes
+#include <Wire.h>  //pour l'EEPROM, la RTC et l'écran.
+#include "structDate.h" //stucture de la date
 //Déclarations variables globales
 int TempExt, TempInt, TempIntMin, TempIntMax,TempExtMin, TempExtMax ;  //Retour des mesures de températures
 bool FaireMesure;   //Toggle pour déclencher les mesures. En fonction de la période des mesures
 int TrigHygro, TrigTemp, TrigLumiere;  //valeur a partir du moment ou on arrose
-int Mois, Annee,Jour,Heure,Minute;  //En provenance de l'horloge
+//Structure pour lire et écrire dans la RTC
+DateRTC DateMesure;
+struct MesureEEPROM //Structure pour la lecture ou l'écriture d'une mesure
+{
+  int TypeMesure;
+  byte HeureEEPROM;
+  byte MinuteEEPROM;
+  byte MoisEEPROM;
+  byte JourEEPROM;
+  byte AnneeEEPROM;
+  int ValMesure; 
+};
 
 //Déclarations des constantes globales
 
@@ -27,16 +40,31 @@ int Mois, Annee,Jour,Heure,Minute;  //En provenance de l'horloge
 #define BoutonGauche 3
 #define BoutonDroit 4
 #define BoutonBas 5
+
 //sorties
 
 //Bus
 
 //déclarations fonctions
+void RecupereDateHeure(DateRTC *date)
+{
+  //Récupère la date de la RTC
+}
+//Fonctions nécessaires pour la RTC
+byte BCDToDec(byte bcd)
+{
+  return((bcd /16*10)+(bcd%16));
+}
+byte DecToBCD(byte dec)
+{
+  return((dec/10*16)+(dec%10));
+}
 void MesureTemp()  //Mesure la températures
 {
   //Mesure la température intérieure, extérieure. Compare avec le min et le max, et si valeurdépassée, stocke en EEPROM
   //Stocke les valeurs dans les variables globales, pour les fonctions de traitement
-  
+ MesureEEPROM MesureTemp;  //Génère une structure type mesure
+  //Récupère la date et l'heure du RTC
 }
 void GestionAreoChauffage()
 {
@@ -91,11 +119,12 @@ void setup() {
   pinMode(BoutonBas, INPUT_PULLUP);
 //initialise les affichages
 //initialise l'horloge (en provenance de l'horloge)
-  Mois=0;
+  RecupereDateHeure(&DateMesure);
+ /* Mois=0;
   Jour=0;
   Heure=0;
   Minute=0;
-  Annee=0;
+  Annee=0;*/
 
 //Lecture de l'eeprom et mise en place des min max
   TempIntMin=0;
@@ -103,7 +132,7 @@ void setup() {
   TempExtMin=0;
   TempExtMax=0;
 
-//Damande a faire une mesure : initialise donc le cycle
+//Demande a faire une mesure : initialise donc le cycle
 }
 
 void loop() {
