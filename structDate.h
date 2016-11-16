b89d9a6a055df391f892f4e7ef3c91b92f44cf74
guillaumeC37 <warthog@free.fr>
2016-11-16 23:19:44 +0100

    Suppression index jour

--- structDate.h
+++ structDate.h
@@ -12,7 +12,6 @@ typedef struct {
   uint8_t secondes;
   uint8_t minutes;
   uint8_t heures; // format 24h
-  uint8_t jourDeLaSemaine; // 1~7 => lundi, mardi, ...
   uint8_t jour;
   uint8_t mois; 
   uint8_t annee; // format yy (ex 2012 -> 12)
